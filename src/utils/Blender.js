8ccb2a5053b6be6879270d49c746c35757103dc1
Miroslavv99 <mbochko99@gmail.com>
2026-01-30 15:52:42 +0200

    feat: added blender func for to shuffle the Pokemon array

---
+++ src/utils/Blender.js
@@ -0,0 +1,9 @@
+export function blender(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let randomIndex = Math.floor(Math.random() * (i + 1));
+
+    [arr[i], arr[randomIndex]] = [arr[randomIndex], arr[i]];
+  }
+
+  return arr;
+}
